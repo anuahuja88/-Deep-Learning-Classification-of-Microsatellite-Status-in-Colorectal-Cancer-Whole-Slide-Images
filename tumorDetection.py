27b4d6d577115234d128150d82358abedc1a410a
anu <aah109@uclive.ac.nz>
2024-05-14 14:25:26 +1200

    finishing preprocessing

--- tumorDetection.py
+++ tumorDetection.py
@@ -14,15 +14,31 @@ for gpu in gpus:
 data = tf.keras.utils.image_dataset_from_directory(data_set)
 data_iterator = data.as_numpy_iterator()
 batch = data_iterator.next()
-
-
-print("Batch shape:", batch[0].shape) 
-print("Labels:", batch[1])  
-
 # Save the first image to disk
 cv2.imwrite("test_image.png", batch[0][0])
 test = cv2.imread("test_image.png")
 converted = cv2.cvtColor(test, cv2.COLOR_RGB2BGR)
-cv2.imshow('test_image.png', converted)
+cv2.imshow('TEST', converted)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+scaled_data = data.map(lambda x, y: (x /255, y))
+scaled_batch = scaled_data.as_numpy_iterator().next()
+
+print(len(data))
+train_size = int(len(scaled_data) * 0.7)
+val_size = int(len(scaled_data) * 0.2)
+test_size = int(len(scaled_data) * 0.1) + 1
+
+
+train = scaled_data.take(train_size)
+val = scaled_data.skip(train_size).take(val_size)
+test = scaled_data.skip(train_size + val_size).take(test_size)
+
+
+
+print("Batch shape:", scaled_batch[0].shape) 
+print("Labels:", scaled_batch[1])  
+
+
